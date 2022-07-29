69593520430fdf02dd4a54f35832012ecef6e9fd
5nam <ohnam90@gmail.com>
2022-07-29 20:20:01 +0900

    7/29
    1. 바텀 네비게이션 시도했으나 실패..
    
    2. menu 만들어놓음

--- app/src/main/java/com/example/healthypleasureapp/Calendar_mainActivity.kt
+++ app/src/main/java/com/example/healthypleasureapp/Calendar_mainActivity.kt
@@ -5,14 +5,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CalendarView
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Calendar_mainActivity : AppCompatActivity() {
+
+    private val fl: FrameLayout by lazy {
+        findViewById(R.id.fl_con)
+    }
+
     lateinit var calendarView : CalendarView
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_main)
+
         calendarView = findViewById(R.id.calendarView)
 
 
